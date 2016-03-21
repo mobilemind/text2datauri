e9c2657aa6d03e6a1c444a011ad2ee61e21f3da2
Tom King <mobilemind@pobox.com>
2016-03-21 15:25:22 -0700

    tasks/*.js,test/*.js: use es6 `let` or `const` instead of `var`

--- test/text2datauri_Encoding.js
+++ test/text2datauri_Encoding.js
@@ -1,5 +1,5 @@
 "use strict";
-var text2datauriHelpers = require('../tasks/text2datauriHelpers.js');
+let text2datauriHelpers = require('../tasks/text2datauriHelpers.js');
 /*
   ======== A Handy Little Nodeunit Reference ========
   https://github.com/caolan/nodeunit
@@ -29,52 +29,52 @@ exports['text2datauri'] = {
 	test.expect(6);
 
 	// test encoding null, null data string
-	var encoding = '';
-	var testVal = '';
-	var expectedVal = '';
-    test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
-      expectedVal,
-      'text2data "' + testVal + '" (null encoding value) should return "' + expectedVal + '"');
+	let encoding = '';
+	let testVal = '';
+	let expectedVal = '';
+	test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
+		expectedVal,
+		'text2data "' + testVal + '" (null encoding value) should return "' + expectedVal + '"');
 
 	// test encoding null, sample data String
 	encoding = '';
 	testVal = ';/?:@&=+," "[]\'\\%<>|';
 	expectedVal = 'Oy8/OkAmPSssIiAiW10nXCU8Pnw=';
-    test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
-      expectedVal,
-      'text2data "' + testVal + '" (null encoding value) should return "' + expectedVal + '"');
+	test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
+		expectedVal,
+		'text2data "' + testVal + '" (null encoding value) should return "' + expectedVal + '"');
 
 	// test encoding base64, null data string
-    encoding = 'base64';
-    testVal = '';
-	expectedVal = '';
-    test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
-      expectedVal,
-      'text2data "' + testVal + '" (base64 encoding) should return "' + expectedVal + '"');
-
-	// test encoding base64, sample data String
-    encoding = 'base64';
-	testVal = ';/?:@&=+," "[]\'\\%<>|';
-	expectedVal = 'Oy8/OkAmPSssIiAiW10nXCU8Pnw=';
-    test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
-      expectedVal,
-      'text2data "' + testVal + '" (base64 encoding) should return "' + expectedVal + '"');
-
-	// test encoding uri, null data string
-    encoding = 'uri';
+	encoding = 'base64';
 	testVal = '';
 	expectedVal = '';
-    test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
-      expectedVal,
-      'text2data "' + testVal + '" (uri encoding) should return "' + expectedVal + '"');
+	test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
+		expectedVal,
+		'text2data "' + testVal + '" (base64 encoding) should return "' + expectedVal + '"');
+
+	// test encoding base64, sample data String
+	encoding = 'base64';
+	testVal = ';/?:@&=+," "[]\'\\%<>|';
+	expectedVal = 'Oy8/OkAmPSssIiAiW10nXCU8Pnw=';
+	test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
+		expectedVal,
+		'text2data "' + testVal + '" (base64 encoding) should return "' + expectedVal + '"');
+
+	// test encoding uri, null data string
+	encoding = 'uri';
+	testVal = '';
+	expectedVal = '';
+	test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
+		expectedVal,
+		'text2data "' + testVal + '" (uri encoding) should return "' + expectedVal + '"');
 
 	// test encoding uri, sample data string
-    encoding = 'uri';
+	encoding = 'uri';
 	testVal = ';/?:@&=+," "[]\'\\%<>|';
 	expectedVal = "%3B%2F%3F%3A%40%26%3D%2B%2C%22%20%22%5B%5D'%5C%25%3C%3E%7C";
-    test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
-      expectedVal,
-      'text2data "' + testVal + '" (uri encoding) should return "' + expectedVal + '"');
+	test.deepEqual(text2datauriHelpers.text2data(testVal, encoding),
+		expectedVal,
+		'text2data "' + testVal + '" (uri encoding) should return "' + expectedVal + '"');
 
    test.done();
   }
