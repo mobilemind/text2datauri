cfb95a21598d3d169af1338d34de58c12d054def
Tom King <mobilemind@pobox.com>
2017-02-02 18:14:05 -0800

    tasks/text2datauri.js: remove unused "text2datauri_pkgVersion"; address other eslint warings

--- tasks/text2datauri.js
+++ tasks/text2datauri.js
@@ -18,11 +18,6 @@ module.exports = function(grunt) {
   // TASKS
   // ==========================================================================
 
-  // default version to  metadata version OR pkg.version if available
-  let text2datauri_pkgVersion = '';
-  if (undefined !== grunt.config('meta.version')) text2datauri_pkgVersion = grunt.config('meta.version');
-  else if (undefined !== grunt.config('pkg.version')) text2datauri_pkgVersion = grunt.config('pkg.version');
-
   // main function
   grunt.registerMultiTask('text2datauri', 'Convert file to data: URI.', function() {
     // process js file to create URI
@@ -30,31 +25,30 @@ module.exports = function(grunt) {
       // set config options
       configOptions = this.options({
         // Default options
-        sourceCharset: 'utf-8', // or 'ascii'; actual format is not validated by text2datauri
-        protocol: 'data:', // any string; this is not validated by text2datauri
-        mimeType: 'text/html', // any string;  this is not validated by text2datauri
-        targetCharset: 'utf-8', // currently only utf-8 is valid
-        encoding: 'base64' // or 'uri'; use 'uri' for encodeURIComponent() encoding
+        "sourceCharset": 'utf-8', // or 'ascii'; actual format is not validated by text2datauri
+        "protocol": 'data:', // any string; this is not validated by text2datauri
+        "mimeType": 'text/html', // any string;  this is not validated by text2datauri
+        "targetCharset": 'utf-8', // currently only utf-8 is valid
+        "encoding": 'base64' // or 'uri'; use 'uri' for encodeURIComponent() encoding
       });
 
       // build prefix
       const uriPrefix = text2datauriHelpers.text2dataPrefix(configOptions);
 
       // loop through files
-      let files = this.files;
+      const files = this.files;
       files.forEach(function(filepair) {
-        let rawStr = grunt.file.read(filepair.src);
+        const rawStr = grunt.file.read(filepair.src);
         // here's where ascii/utf-8 might be validated or pre-processed
         // encode rawStr to data value
-        let dataStr = text2datauriHelpers.text2data(rawStr, configOptions.encoding);
+        const dataStr = text2datauriHelpers.text2data(rawStr, configOptions.encoding);
 
         // write full URI to file & log
-        let fullURI = uriPrefix + dataStr;
+        const fullURI = uriPrefix + dataStr;
         grunt.file.write(filepair.dest, fullURI);
         console.log(filepair.src + ' -> ' + filepair.dest + ' (' + dataStr.length + ' bytes of data, ' + fullURI.length + ' total bytes)');
       });
-    }
-    catch(e) {
+    } catch(e) {
       // warn on error
       grunt.warn(this.name + ' found errors: ' + e.message, 10);
       return false;
